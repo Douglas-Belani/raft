e96432d96d69bf4981a5b99979a2abd34f214301
Douglas Belani <douglas.belani@gmail.com>
2021-12-06 21:38:15 -0300

    Fixing script start-app.sh

--- scripts/start-app.sh
+++ scripts/start-app.sh
@@ -14,7 +14,7 @@ fi
 
 APP_NAME="rest-api-file-transfer-0.0.1-SNAPSHOT.jar"
 
-nohup -Dlog4j.configurationFile=./config/logback.xml java -jar ./lib/${APP_NAME} &
+nohup java -Dlog4j.configurationFile=./config/logback.xml -jar ./lib/${APP_NAME} &
 
 PID=$(ps -ef | grep "${APP_NAME}" | grep -v grep | awk '{print $2}')
 
